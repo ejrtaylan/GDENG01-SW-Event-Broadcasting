eb8fd08c774dd06659c4d132f24db79eb5c560f4
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-05-22 21:18:25 +0800

    Some updates

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSpawnCubeClicked(){
+        Debug.Log("Spawn Cubes Clicked");
+    }
+
+    public void OnSpawnBallClicked(){
+        Debug.Log("Spawn Ball Clicked");
+
+    }
+
+    public void OnClearAllClicked(){
+        Debug.Log("Clear All Clicked");
+
+    }
+}
